69dc2f15571e7edd322adaf0087a6eb3c4b9f358
Kenneth Liang <kennethliang0@gmail.com>
2019-05-02 11:00:56 -0400

    Fix .gitignore. Added an additional check.

--- app/src/main/java/com/kennethliang/splitscreenhelper/SplitScreenAccessibilityService.java
+++ app/src/main/java/com/kennethliang/splitscreenhelper/SplitScreenAccessibilityService.java
@@ -3,6 +3,7 @@ package com.kennethliang.splitscreenhelper;
 import android.accessibilityservice.AccessibilityService;
 import android.util.Log;
 import android.view.accessibility.AccessibilityEvent;
+import android.widget.Toast;
 
 public class SplitScreenAccessibilityService extends AccessibilityService {
 
@@ -13,7 +14,9 @@ public class SplitScreenAccessibilityService extends AccessibilityService {
         switch (event.getEventType()) {
             case AccessibilityEvent.TYPE_VIEW_LONG_CLICKED:
                 if (event.getContentDescription().toString().equals("Overview")) {
-                    performGlobalAction(GLOBAL_ACTION_TOGGLE_SPLIT_SCREEN);
+                    if (!performGlobalAction(GLOBAL_ACTION_TOGGLE_SPLIT_SCREEN)) {
+                        Toast.makeText(this, "Unable to enter split screen mode", Toast.LENGTH_SHORT).show();
+                    }
                 }
                 break;
         }
